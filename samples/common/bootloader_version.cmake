5277c3e16e600881116fa5aa833220bd6e7250de
Nima Sarrafi <nimasarrafi@gmail.com>
2024-02-25 15:20:57 -0800

    fix bug for cmake 3.28

--- samples/common/bootloader_version.cmake
+++ samples/common/bootloader_version.cmake
@@ -1,5 +1,6 @@
-find_package(PythonInterp 3.6 REQUIRED)
-find_package(Python 3.6 REQUIRED)
+# workaround for newer cmake versions
+# find_package(PythonInterp 3.6 REQUIRED)
+# find_package(Python 3.6 REQUIRED)
 
 execute_process(COMMAND ${PYTHON_EXECUTABLE} $ENV{ZEPHYR_BASE}/../sidewalk/scripts/generate_version_info_overlay.py
 OUTPUT_FILE ${CMAKE_CURRENT_SOURCE_DIR}/version.conf)
